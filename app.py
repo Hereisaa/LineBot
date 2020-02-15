d8be0118a9c2bcaef98c47bf8026b33d64a9302d
awei1995 <a0979096278@gmail.com>
2020-02-15 09:34:27 +0800

    +crawler TOP 5

--- app.py
+++ app.py
@@ -36,21 +36,21 @@ def callback():
 
     return 'OK'
 
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    #print("Handle: reply_token: " + event.reply_token + ", message: " + event.message.text)
-    #content = "{}: {}".format(event.source.user_id, event.message.text)
-    content = "{}".format(event.message.text)
+# @handler.add(MessageEvent, message=TextMessage)
+# def handle_message(event):
+#     #print("Handle: reply_token: " + event.reply_token + ", message: " + event.message.text)
+#     #content = "{}: {}".format(event.source.user_id, event.message.text)
+#     content = "{}".format(event.message.text)
 
-    if content[3:] == "口罩":
-        line_bot_api.reply_message(event.reply_token,
-            TextSendMessage(text=mask_crawler.reply(content,'text')))
-    elif content[3:] != "口罩":
-        line_bot_api.reply_message(event.reply_token,
-            TextSendMessage(text='輸入格式錯誤'))
-    else:
-        line_bot_api.reply_message(event.reply_token,
-            TextSendMessage(text=content))
+#     if content[3:] == "口罩":
+#         line_bot_api.reply_message(event.reply_token,
+#             TextSendMessage(text=mask_crawler.reply(content,'text')))
+#     elif content[3:] != "口罩":
+#         line_bot_api.reply_message(event.reply_token,
+#             TextSendMessage(text='輸入格式錯誤'))
+#     else:
+#         line_bot_api.reply_message(event.reply_token,
+#             TextSendMessage(text=content))
 
 @handler.add(MessageEvent, message=LocationMessage)
 def handle_location_message(event):
@@ -72,30 +72,7 @@ def handle_location_message(event):
 
     line_bot_api.reply_message(event.reply_token, send_message)
 
-    # line_bot_api.reply_message(
-    #     event.reply_token,
-    #     [
-    #         LocationSendMessage(
-    #             title=info[0], 
-    #             # 地址 電話 成人數量 兒童數量 距離
-    #             address="{}\n{}\n成人剩餘  {}個\n兒童剩餘  {}個\n與您距離  {} km"
-    #                     .format(addr, info[1], info[2], info[3], info[4]),
-    #             latitude=info[5], 
-    #             longitude=info[6]
-    #         ),
-    #                     LocationSendMessage(
-    #             title=info[0], 
-    #             # 地址 電話 成人數量 兒童數量 距離
-    #             address="{}\n{}\n成人剩餘  {}個\n兒童剩餘  {}個\n與您距離  {} km"
-    #                     .format(addr, info[1], info[2], info[3], info[4]),
-    #             latitude=info[5], 
-    #             longitude=info[6]
-    #         )
-    #     ]
-    # )
-
-
-
+## Carousel template
 # @handler.add(MessageEvent, message=LocationMessage)
 # def handle_location_message(event):
 #     image_url = 'https://cdn.hk01.com/di/media/images/564720/org/7a5b31ccd89a2360794c1ef6bf54393f.jpg/0ws2YFTJcguqJ5hF1Hp3V8ELwZfAP_rMiLU2UYi1NlE?v=w1920'
